059ade52aa76ca5cd714949e682b458ed11eb160
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-06-09 23:36:42 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -36,8 +36,8 @@ function getComps(sort) {
    let url;
    if (leaderId == null) url = `${server}/comps/search/${sort}/${chIds}`;
    else {
-      const deckName = `${getCharacter(Number(leaderId))}덱`;
-      url = `${server}/comps/searchWithLeader/${sort}/${chIds}/${deckName}`;
+      const leader = getCharacter(Number(leaderId));
+      url = `${server}/comps/searchWithLeader/${sort}/${chIds}/${leader.name}덱`;
    }
 
    request(url, {
