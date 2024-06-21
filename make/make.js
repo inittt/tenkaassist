f1973896feb652e54cbb9bac74cb53abebc29155
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-06-21 21:20:16 +0900

    Update make.js

--- make/make.js
+++ make/make.js
@@ -198,7 +198,7 @@ function makeBlockNDeck() {
                <div class="character" style="margin:0.2rem;">
                   <div style="margin:0.2rem;">
                      <img src="${address}/images/characters/cs${ch.id}_0_0.webp" class="img z-1" alt="">
-                     ${Math.floor(id/10000) == 9 ? "" : `<img src="${address}/images/icons/ro_${ch.role}.webp" class="el-icon z-2">`}
+                     ${Math.floor(ch.id/10000) == 9 ? "" : `<img src="${address}/images/icons/ro_${ch.role}.webp" class="el-icon z-2">`}
                      <div class="element${ch.element} ch_border z-4"></div>
                   </div>
                   <div class="text-mini">${ch.name}</div>
