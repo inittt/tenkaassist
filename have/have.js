f5ca1730087464495af13028d6565e115f673ddb
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-06-24 20:38:37 +0900

    Update have.js

--- have/have.js
+++ have/have.js
@@ -48,12 +48,13 @@ function getCharactersWithCondition(element, role, rarity, search) {
 
 // 검색 버튼 누를시
 function searchDeck() {
+   const go = [...selected];
    if (selected.length < 1) return alert("하나 이상의 캐릭터를 선택해 주세요");
    for(const ch of chJSON.data) {
       if (ch.rarity == 3) continue;
-      if (selected.indexOf(ch.id) == -1) selected.push(ch.id);
+      if (go.indexOf(ch.id) == -1) go.push(ch.id);
    }
-   location.href = `${address}/make/?list=${selected}`;
+   location.href = `${address}/make/?list=${go}`;
 }
 
 function resizeButton() {
