1e888f7e24ba4da251c6a8ba2f729b3cd2ba8a13
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-07-18 23:45:22 +0900

    Update calculator.js

--- simulator/calculator.js
+++ simulator/calculator.js
@@ -5744,30 +5744,18 @@ function setDefault(me) {switch(me.id) {
       me.ultimate = function() {ultLogic(me);
          if (me.isLeader) {
             // 자신 이외의 아군 전체는 행동 후 "자신 이외의 아군 전체의 <특제-마물요리>의 모든 효과 제거" 발동(2턴)(1회 발동 후 해제)
-            for(let c of comp) if (c.id != me.id) {
-               for(let c2 of comp) if (c2.id != c.id) {
-                  atbf(c, "행동", c2, "제거", "기본", "<특제-마물요리>", 1, 2);
-               }
-            }
-            for(let c of comp) if (c.id != me.id) {
-               for(let c2 of comp) if (c2.id != me.id) {
-                  atbf(c, "행동", c2, "제거", "발동", "<특제-마물요리>", 1, 2);
-               }
-            }
+            for(let c of comp) if (c.id != me.id) for(let c2 of comp) if (c2.id != c.id)
+               atbf(c, "행동", c2, "제거", "기본", "<특제-마물요리>", 1, 2);
+            for(let c of comp) if (c.id != me.id) for(let c2 of comp) if (c2.id != me.id)
+               atbf(c, "행동", c2, "제거", "발동", "<특제-마물요리>", 1, 2);
          }
          
          // 궁극기 발동 시, "자신 이외의 아군 전체는 <즐거운 만찬> 획득" 발동
          // 행동 후 "자신 이외의 아군 전체의 <즐거운 만찬>의 모든 효과 제거" 발동(2턴)(1회 발동 후 해제)
-         for(let c of comp) if (c.id != me.id) {
-            for(let c2 of comp) if (c2.id != c.id) {
-               atbf(c, "행동", c2, "제거", "기본", "<즐거운 만찬>", 1, 2);
-            }
-         }
-         for(let c of comp) if (c.id != me.id) {
-            for(let c2 of comp) if (c2.id != me.id) {
-               atbf(c, "행동", c2, "제거", "발동", "<즐거운 만찬>", 1, 2);
-            }
-         }
+         for(let c of comp) if (c.id != me.id) for(let c2 of comp) if (c2.id != c.id)
+            atbf(c, "행동", c2, "제거", "기본", "<즐거운 만찬>", 1, 2);
+         for(let c of comp) if (c.id != me.id) for(let c2 of comp) if (c2.id != me.id)
+            atbf(c, "행동", c2, "제거", "발동", "<즐거운 만찬>", 1, 2);
       };
       me.atkbefore = function() {}
       me.atkafter = function() {}
