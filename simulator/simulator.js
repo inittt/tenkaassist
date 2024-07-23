ea17d0d78d3cdf00db6f7e20479a2d0c3096ccb8
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-07-23 22:39:10 +0900

    Update simulator.js

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -89,6 +89,8 @@ function start(compIds) {
    comp[0].leader();
    for(let i = 0; i < 5; i++) comp[i].passive();
    for(let i = 0; i < 5; i++) comp[i].turnstart();
+
+   savedData.length = 0;
    updateAll();
 }
 
