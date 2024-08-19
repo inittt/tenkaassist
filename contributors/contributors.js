722b68b8fed76774b44aa82a8ad2ce102c8a2b2a
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-08-19 22:44:28 +0900

    update

--- contributors/contributors.js
+++ contributors/contributors.js
@@ -3,7 +3,6 @@ const curHeader = 7;
 document.addEventListener("DOMContentLoaded", function() {
    loadGameUser();
    loadAddUser();
-   loadUpdateUser();
 });
 
 function loadGameUser() {
@@ -52,31 +51,6 @@ function loadAddUser() {
          `)
       }
       document.getElementById("add-user").innerHTML = users.join("");
-   }).catch(e => {
-      alert("데이터 로드 실패", e);
-   })
-}
-function loadUpdateUser() {
-   request(`${server}/users/get/contributors/2`, {
-      method: "GET",
-   }).then(response => {
-      if (!response.ok) throw new Error('네트워크 응답이 올바르지 않습니다.');
-      return response.json();
-   }).then(res => {
-      if (!res.success) return alert("데이터 로드 실패");
-      const users = []
-      let cnt = 0;
-      for(const u of res.data) {
-         if (u.updatecount == 0 || cnt == 10) break;
-         users.push(`
-            <div class="user-contribution-text">
-               <span class="user-order">#${++cnt}</span>
-               <span>${u.name}</span>
-               <span class="user-point">${u.updatecount}</span>
-            </div>
-         `)
-      }
-      document.getElementById("update-user").innerHTML = users.join("");
    }).catch(e => {
       alert("데이터 로드 실패", e);
    })
