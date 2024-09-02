cb0393196961e002c61d1f8346777dfaaa7acf08
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-09-02 23:25:51 +0900

    update

--- search/search.js
+++ search/search.js
@@ -106,8 +106,8 @@ function makeBlock(sort) {
       let last;
       switch(sort) {
          case 1 : last = `<i class="fa-solid fa-burst"></i> ${formatNumber(recommend)}`; break;
-         case 2 : last = `<i class="fa-solid fa-skull"></i> ${rankOrBond1(ranking, vote)}`; break;
-         case 3 : last = `<i class="fa-solid fa-skull"></i> ${rankOrBond1(ranking, vote)}`; break;
+         case 2 : last = `${rankOrBond1(ranking, vote)}`; break;
+         case 3 : last = `${rankOrBond1(ranking, vote)}`; break;
          case 4 : last = `<i class="fa-solid fa-burst"></i> ${formatNumber(vote)}`; break;
          default : last = `<i class="fa-solid fa-skull"></i> ${ranking.toFixed(0)}${t("턴")}`;
       } stringArr.push(`</div><div class="comp-rank">${last}</div></div>`);
@@ -127,8 +127,8 @@ function makeBlock(sort) {
 }
 
 function rankOrBond1(ranking, dmg13_1) {
-   if (ranking < 99 || dmg13_1 == 0) return ranking.toFixed(0)+t("턴");
-   return formatNumber(dmg13_1)+" (1)";
+   if (ranking < 99 || dmg13_1 == 0) return `<i class="fa-solid fa-skull"></i> `+ranking.toFixed(0)+t("턴");
+   return `<i class="fa-solid fa-burst"></i> `+formatNumber(dmg13_1)+" (1)";
 }
 
 function init() {
