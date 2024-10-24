5f2cb72466f69bf5292003d2582121de1bb10eae
inittt <141410295+inittt@users.noreply.github.com>
2024-10-24 14:13:41 +0900

    have.js 업데이트

--- have/have.js
+++ have/have.js
@@ -90,6 +90,7 @@ function getCharactersWithCondition(element, role, rarity, search) {
 
 // 검색 버튼 누를시
 function searchDeck() {
+return alert("현재 사용 불가");
    request(`${server}/users/me`, {
       method: "GET",
    }).then(response => {
