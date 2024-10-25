d78220bcbb000d950f7f3bcc0539e6d1eff531e8
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-10-25 23:21:18 +0900

    Update compAdd.js

--- comp/add/compAdd.js
+++ comp/add/compAdd.js
@@ -69,8 +69,8 @@ function registerDeck() {
    }).then(res => {
       if (!res.success) return alert(res.msg);
       alert(
-         t("등록 성공")+"\n\n"+
-         t("데미지 측정을 하지 않거나 13턴 데미지(5)가 40억 미만인 조합은 주기적으로 삭제됩니다")
+         t("등록 성공")
+         //+"\n\n"+t("데미지 측정을 하지 않거나 13턴 데미지(5)가 40억 미만인 조합은 주기적으로 삭제됩니다")
       );
       location.href = `${address}/comp/?id=${res.data}`
    }).catch(e => {
