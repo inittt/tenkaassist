503b992a3614d39dc10286517408cb245e31950c
kdjdj77 <141410295+inittt@users.noreply.github.com>
2024-12-12 21:52:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function makeBlock(data, sort) {
                <div style="margin:0.2rem;">
                   <img src="${address}/images/characters/cs${ch.id}_0_0.webp" class="img z-1" alt="">
                   <img src="${address}/images/icons/ro_${ch.role}.webp" class="el-icon z-2">
-                  ${leaderHpOn ? `<div class="hpbox" z-2"><img class="i-heart" src="../images/icons/ico-heart.svg">${ch.hpUp ? ch.hpUp : 0}</div>` : ""}
+                  ${leaderHpOn ? `<div class="hpbox" z-2"><img class="i-heart" src="./images/icons/ico-heart.svg">${ch.hpUp ? ch.hpUp : 0}</div>` : ""}
                   ${liberationList.includes(ch.name) ? `<img src="${address}/images/icons/liberation.webp" class="li-icon z-2">` : ""}
                   <div class="element${ch.element} ch_border z-4"></div>
                </div>
