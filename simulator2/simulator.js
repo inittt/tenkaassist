c707b0c64ea1f931c30f2a2a4773b049294aeefd
kdjdj77 <141410295+inittt@users.noreply.github.com>
2025-06-10 00:22:46 +0900

    Update simulator.js

--- simulator2/simulator.js
+++ simulator2/simulator.js
@@ -282,10 +282,8 @@ function updateAll() {
    getdiv("cumulative-dmg").innerHTML = (boss.maxHp - Math.floor(boss.hp)).toLocaleString();
    if (boss.def) {
       document.getElementById("bossdef").style.display = "inline";
-      document.getElementById("endf").style.borderColor = "white";
    } else {
       document.getElementById("bossdef").style.display = "none";
-      document.getElementById("endf").style.borderColor = "transparent";
    }
 }
 function updateCdBar(i) {
