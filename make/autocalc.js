520cb5c8e90bfcec4c711a46524ac261580c3dfb
kdjdj77 <141410295+inittt@users.noreply.github.com>
2025-06-12 15:05:07 +0900

    Update autocalc.js

--- make/autocalc.js
+++ make/autocalc.js
@@ -18,6 +18,7 @@ function autoCalc(idList, command, bondList, boss_element = -1) {
       boss.hp = boss.maxHp; boss.def = false;
       boss.buff = []; boss.li = []; alltimeFunc.length = 0;
       buff_ex.length = 0;
+      buff_ex.push("도트뎀", "제거");
 
       for(const id of compIds) {
          const tmp = chJSON.data.filter(ch => ch.id === id)[0];
